a866a81ab64e267d289d8c84c0b4b1f94799b212
masun.ding <masun.ding@xthings.com>
2025-03-26 17:54:32 +0800

    feat: add route

--- Sources/SwiftRouteX/SwiftRouteX.swift
+++ Sources/SwiftRouteX/SwiftRouteX.swift
@@ -1,2 +1,42 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import Combine
+
+@MainActor
+public class RouterX: ObservableObject {
+    
+    enum Route{
+        case navigation
+        case sheet
+        case fullScreen
+        case modal
+    }
+
+    struct State{
+//        var navigationPath: [ANRouterPathView] = []
+//        var presentingSheet: ANRouterPathView? = nil
+//        var presentingFullScreen: ANRouterPathView? = nil
+//        var presentingModal: ANRouterPathView? = nil
+//        var root: Binding<ANRouterPathView?>
+//        
+//        var isPresenting: Bool {
+//            presentingSheet != nil || presentingFullScreen != nil || presentingModal != nil
+//        }
+    }
+    
+    @Published private(set) var state: State
+    
+    public init(state: State) {
+        self.state = state
+    }
+    
+//    public init(root: Binding<ANRouterPathView?>) {
+//        state = State(root: root)
+//    }
+//    
+//    func view(path: ANRouterPathView, route: Route) -> AnyView {
+//        return path.view
+//    }
+}
