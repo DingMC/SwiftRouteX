b0173dfcf145c279657a9f757d3a31e81657d464
masun.ding <masun.ding@xthings.com>
2025-03-26 16:34:51 +0800

    create project

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftRouteX",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftRouteX",
+            targets: ["SwiftRouteX"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftRouteX"),
+        .testTarget(
+            name: "SwiftRouteXTests",
+            dependencies: ["SwiftRouteX"]
+        ),
+    ]
+)
